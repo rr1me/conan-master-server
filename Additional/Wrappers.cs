8d8c19e8176fa4c8a0f79114ee9db314dccfea4c
rime <s1337228@yandex.ru>
2023-03-03 04:37:56 +0300

    wrappers class

---
+++ Additional/Wrappers.cs
@@ -0,0 +1,30 @@
+﻿using conan_master_server.Entities;
+
+namespace conan_master_server.Additional;
+
+class funcWrap
+{
+    public object FunctionResult { get; }
+
+    public funcWrap(object functionResult)
+    {
+        FunctionResult = functionResult;
+    }
+}
+
+class cloudResp
+{
+    public bool IsPlayerBanned { get; } = false;
+    public object ChargebackAdjustedVirtualCurrencies { get; } = new { };
+    public object BannedNegativeVirtualCurrencies { get; } = new { };
+}
+
+class sessions
+{
+    public List<ServerEntity> Sessions { get; }
+
+    public sessions(List<ServerEntity> sessions)
+    {
+        Sessions = sessions;
+    }
+}
--- Controllers/ConanController.cs
+++ Controllers/ConanController.cs
@@ -106,30 +106,3 @@ public class ConanController : ControllerBase
         _cleanerOrchestrator.Run();
     }
 }
-
-class funcWrap
-{
-    public object FunctionResult { get; }
-
-    public funcWrap(object functionResult)
-    {
-        FunctionResult = functionResult;
-    }
-}
-
-class cloudResp
-{
-    public bool IsPlayerBanned { get; } = false;
-    public object ChargebackAdjustedVirtualCurrencies { get; } = new { };
-    public object BannedNegativeVirtualCurrencies { get; } = new { };
-}
-
-class sessions
-{
-    public List<ServerEntity> Sessions { get; }
-
-    public sessions(List<ServerEntity> sessions)
-    {
-        Sessions = sessions;
-    }
-}
